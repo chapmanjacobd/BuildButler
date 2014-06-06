d1b444f98dc59c6171dbeff6265d3d37d3a267ea
Chris Chapman <chris@pentandra.com>
2014-06-05 18:20:43 -0600

    Much improvement
    * Use strict mode (encapsulated in a function)
    * select paths
    * refactor a lot of stuff

--- bb/zglobal/js/bbutler.js
+++ bb/zglobal/js/bbutler.js
@@ -5,259 +5,274 @@
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-var bbutler = bbutler || {};
+var buildButler = (function(bbutler, window, document) {
 
-bbutler.Helpers = (function() {
-  var pub = {};
+  'use strict';
 
-  pub.toggleClass = function(el, className) {
-    if (el.classList) {
-      el.classList.toggle(className);
-    } else {
-      // This implementation works for >= IE9
-      var classes = el.className.split(' ');
-      var existingIndex = classes.indexOf(className);
+  bbutler.Helpers = (function() {
 
-      if (existingIndex >= 0)
-        classes.splice(existingIndex, 1);
-      else
-        classes.push(className);
+    var pub = {};
 
-      el.className = classes.join(' ');
-    }
-  }
-
-  pub.addClass = function(el, className) {
-    if (el.classList)
-      el.classList.add(className);
-    else
-      el.className += ' ' + className;
-  }
-
-  pub.removeClass = function(el, className) {
-    if (el.classList)
-      el.classList.remove(className);
-    else
-      el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-  }
-
-  pub.getXml = function(url, mimeType, success) {
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-
-    request.responseType = 'document';
-    request.overrideMimeType(mimeType);
-
-    request.onload = function() {
-      if (request.status >= 200 && request.status < 400){
-        // Success!
-        success(request.responseXML);
+    pub.toggleClass = function(el, className) {
+      if (el.classList) {
+        el.classList.toggle(className);
       } else {
-        // We reached our target server, but it returned an error
-        console.log("oops");
+        // This implementation works for >= IE9
+        var classes = el.className.split(' ');
+        var existingIndex = classes.indexOf(className);
+
+        if (existingIndex >= 0)
+          classes.splice(existingIndex, 1);
+        else
+          classes.push(className);
+
+        el.className = classes.join(' ');
       }
-    };
-
-    request.onerror = function() {
-      console.log("There was a connection error of some sort");
-    };
-
-    request.send();
-  }
-
-  /**
-   * Create an application event. (IE9+)
-   *
-   * @param {string} type The type of event
-   * @param {any} detail Custom information to pass along with the event
-   */
-  pub.createApplicationEvent = function(type, detail) {
-    if (window.CustomEvent)
-      var event = new CustomEvent(type, { bubbles: true, cancelable: true, detail: detail });
-    else {
-      var event = document.createEvent('CustomEvent');
-      event.initCustomEvent(type, true, true, detail);
     }
 
-    return event;
-  }
+    pub.addClass = function(el, className) {
+      if (el.classList)
+        el.classList.add(className);
+      else
+        el.className += ' ' + className;
+    }
 
-  return pub;
-})();
+    pub.removeClass = function(el, className) {
+      if (el.classList)
+        el.classList.remove(className);
+      else
+        el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    }
 
-bbutler.PartSelection = (function(helpers) {
+    pub.getXml = function(url, mimeType, success) {
+      var request = new XMLHttpRequest();
+      request.open('GET', url, true);
 
-  var searchField = document.querySelector("#search");
+      request.responseType = 'document';
+      request.overrideMimeType(mimeType);
 
-  var init = function(el, parts) {
+      request.onload = function() {
+        if (request.status >= 200 && request.status < 400){
+          // Success!
+          success(request.responseXML);
+        } else {
+          // We reached our target server, but it returned an error
+          console.log("oops");
+        }
+      };
 
-    searchField.addEventListener('keyup', function(event) {
+      request.onerror = function() {
+        console.log("There was a connection error of some sort");
+      };
 
-    });
-
-  }
-
-  var clearFilter = function() { }
-
-  return {
-    init: init,
-    filter: function(query) {},
-    clearFilter: clearFilter
-  }
-
-})(bbutler.Helpers);
-
-bbutler.Schematic = (function(svgPanZoom, partSelection, helpers) {
-
-  var buildButler = document.querySelector('#build');
-  var selectedPart, panZoomSchematic;
-
-  /**
-   * Fetches an SVG file asynchronously from the server and imports a copy of it into the current document.
-   *
-   * @param {string} the filename of the file to fetch from the server
-   * @returns {Node} The SVG file as a node that has not been inserted yet into the document tree
-   */
-  var importSVG = function(filename, callback) {
-    helpers.getXml(filename, 'image/svg+xml', function(xml) {
-      // if (error) return console.error(error);
-
-      var imported = document.importNode(xml.documentElement, true);
-
-      callback(imported);
-    });
-  }
-
-  /**
-   * Assembles schematic and inserts into the document tree.
-   *
-   */
-  var assembleSchematic = function(el, callback) {
-
-    function withSchematicDimensions(svg) {
-      var rect = svg.createSVGRect();
-      rect.width = parseFloat(svg.getAttribute('width'));
-      rect.height = parseFloat(svg.getAttribute('height'));
-
-      return rect;
+      request.send();
     }
 
     /**
-     * Creates an <image> element of the base schematic.
+     * Create an application event. (IE9+)
      *
-     * @param {SVGRect} the dimensions of the SVG.
-     * @param {String} name of the schematic image file
-     * @returns {Element} an <image> element ready to insert into a document tree
+     * @param {string} type The type of event
+     * @param {any} detail Custom information to pass along with the event
      */
-    function createBaseSchematic(viewportDimensions, filename) {
+    pub.createApplicationEvent = function(type, detail) {
+      if (window.CustomEvent)
+        var event = new CustomEvent(type, { bubbles: true, cancelable: true, detail: detail });
+      else {
+        var event = document.createEvent('CustomEvent');
+        event.initCustomEvent(type, true, true, detail);
+      }
 
-      var baseImage = document.createElementNS('http://www.w3.org/2000/svg', 'image');
-      baseImage.setAttributeNS('http://www.w3.org/1999/xlink', 'href', filename);
-      baseImage.setAttribute('x', 0);
-      baseImage.setAttribute('y', 0);
-      baseImage.setAttribute('width', viewportDimensions.width);
-      baseImage.setAttribute('height', viewportDimensions.height);
-
-      return baseImage;
+      return event;
     }
 
-    function appendSchematic(svg) {
-      var baseSchematicElement = createBaseSchematic(withSchematicDimensions(svg), 'base.svg');
-      svg.insertBefore(baseSchematicElement, svg.firstChild);
-
-      return el.appendChild(svg);
+    pub.contains = function(array, search) {
+      return (array.indexOf(search) >= 0)
     }
 
-    importSVG('build.svg', function(svg) {
-      var schematicElement = appendSchematic(svg);
-      callback(schematicElement);
-    });
-  }
+    return pub;
+  })();
 
-  var getPartsFromSchematic = function(schematicEl) {
+  bbutler.Schematic = (function(svgPanZoom, helpers) {
 
-  }
+    var build = document.querySelector('#build');
 
-  var init = function() {
-    assembleSchematic(buildButler, function(schematic) {
-      panZoomSchematic = svgPanZoom(schematic);
+    var svgNS = 'http://www.w3.org/2000/svg',
+      xlinkNS = 'http://www.w3.org/1999/xlink';
+
+    var selectedPart, panZoomSchematic;
+
+    /**
+     * Fetches an SVG file asynchronously from the server and imports a copy of it into the current document.
+     *
+     * @param {string} filename The filename of the file to fetch from the server
+     * @param {function} callback The callback that will be called (with the imported document node) after the SVG is received
+     */
+    var importSVG = function(filename, callback) {
+      helpers.getXml(filename, 'image/svg+xml', function(xml) {
+        var imported = document.importNode(xml.documentElement, true);
+        callback(imported);
+      });
+    }
+
+    var extractPartNumber = function(htmlId) {
+      return htmlId.indexOf('_') === 0 ? htmlId.substring(1) : htmlId;
+    }
+
+    /**
+     * Assembles schematic and inserts into the document tree.
+     *
+     * @param {function} callback The function to call when the assembly is complete.
+     */
+    var assemble = function(callback) {
+
+      // dirty hack
+      function withSchematicDimensions(svg) {
+        var rect = svg.createSVGRect();
+        rect.width = parseFloat(svg.getAttribute('width'));
+        rect.height = parseFloat(svg.getAttribute('height'));
+
+        return rect;
+      }
+
+      /**
+       * Creates an <image> element of the base schematic.
+       *
+       * @param {SVGRect} rect the dimensions of the SVG.
+       * @param {string} filename the name of the schematic image file
+       * @returns {Element} an <image> element ready to insert into a document tree
+       */
+      function createBaseSchematic(rect, filename) {
+        var baseImage = document.createElementNS(svgNS, 'image');
+        baseImage.setAttributeNS(xlinkNS, 'href', filename);
+        baseImage.setAttribute('x', 0);
+        baseImage.setAttribute('y', 0);
+        baseImage.setAttribute('width', rect.width);
+        baseImage.setAttribute('height', rect.height);
+
+        return baseImage;
+      }
+
+      function appendSchematic(svg) {
+        var baseSchematicElement = createBaseSchematic(withSchematicDimensions(svg), 'base.svg');
+        svg.insertBefore(baseSchematicElement, svg.firstChild);
+
+        return build.appendChild(svg);
+      }
+
+      function setupTooltips(schematic) {
+
+      }
+
+      function registerEventHandlers(schematic) {
+        schematic.addEventListener('click', function(event) {
+          selectPart(event.target);
+        });
+      }
+
+      function setupPanZoom(el) {
+        panZoomSchematic = svgPanZoom(el);
+      }
+
+      importSVG('build.svg', function(imported) {
+        var schematicElement = appendSchematic(imported);
+        setupTooltips(schematicElement);
+        registerEventHandlers(schematicElement);
+        setupPanZoom(schematicElement);
+
+        callback(schematicElement);
+      });
+    }
+
+    var isShape = function (el) {
+      var shapes =  ['path', 'rect', 'circle', 'ellipse', 'line', 'polyline', 'polygon'];
+      return helpers.contains(shapes, el.tagName);
+    }
+
+    var selectPart = function(part) {
+      if (part == null || !isShape(part)) return;
+      if (part === selectedPart) return;
+
+      if (isPartSelected()) helpers.removeClass(selectedPart, 'selectedpart');
+      helpers.addClass(part, 'selectedpart');
+
+      selectedPart = part;
+
+      var event = helpers.createApplicationEvent('bbutler.partselected');
+      part.dispatchEvent(event);
+    }
+
+    var isPartSelected = function() {
+      return selectedPart != null;
+    }
+
+    var selectPartById = function(id) {
+      var part = build.querySelector(id);
+      selectPart(part);
+    }
+
+    var reset = function() {
+      panZoomSchematic.resetZoom();
+      panZoomSchematic.center();
+    }
+
+    return {
+      assemble: assemble,
+      getSelectedPartNumber: function() { return selectedPart ? extractPartNumber(selectedPart.id) : "No part selected"; },
+      title: function() { return build; },
+      reset: reset,
+      selectPartById: selectPartById
+    }
+  })(svgPanZoom, bbutler.Helpers);
+
+  bbutler.PartList = (function(schematic, helpers) {
+
+    var searchField = document.querySelector("#search");
+
+    var getPartsFromSchematic = function(schematic) {
+
+    }
+
+    var init = function() {
 
       var parts = getPartsFromSchematic(schematic);
-      partSelection.init(parts);
-    });
-  }
 
-  var extractId = function(htmlId) {
-    return htmlId.indexOf(0) === '_' ? htmlId.substring(1) : htmlId;
-  }
+      searchField.addEventListener('keyup', function(event) {
 
-  var selectPart = function(id) {
-    var part = buildButler.querySelector(id);
-    if (part === selectedPart) return;
-
-    var event = helpers.createApplicationEvent('bbutler.partselected');
-    part.dispatchEvent(event);
-
-    // if (isPartSelected())
-    //   helpers.removeClass(selectedPart, 'selectedpart');
-
-    // selectedPart = part;
-    // helpers.addClass(part, 'selectedpart');
-  }
-
-  var isPartSelected = function() {
-    return selectedPart !== null;
-  }
-
-  var reset = function() {
-    panZoomSchematic.resetZoom();
-    panZoomSchematic.center();
-  }
-
-  return {
-    init: init,
-    selectedPart: selectedPart,
-    reset: reset,
-    selectPart: function(id) { selectPart(id) }
-  }
-})(svgPanZoom, bbutler.PartSelection, bbutler.Helpers);
-
-bbutler.Main = (function(schematic, helpers) {
-
-  var bindInvertButton = function() {
-    var invertEl = document.querySelector("#invert");
-    invertEl.addEventListener('click', function() {
-      helpers.toggleClass(document.documentElement, "inverted")
-    });
-  }
-
-  var init = function() {
-    schematic.init();
-    bindInvertButton();
-  }
-
-  return {
-    init: init
-  }
-})(bbutler.Schematic, bbutler.Helpers);
-
-//preventdoubletapzoom
-/*
-(function($) {
-  $.fn.nodoubletapzoom = function() {
-      $(this).bind('touchstart', function preventZoom(e) {
-        var t2 = e.timeStamp
-          , t1 = $(this).data('lastTouch') || t2
-          , dt = t2 - t1
-          , fingers = e.originalEvent.touches.length;
-        $(this).data('lastTouch', t2);
-        if (!dt || dt > 500 || fingers > 1) return; // not double-tap
-
-        e.preventDefault(); // double tap - prevent the zoom
-        $(this).trigger('click').trigger('click');
       });
-  };
-})(jQuery);
-*/
 
-document.addEventListener('DOMContentLoaded', bbutler.Main.init);
+    }
+
+    var clearFilter = function() { }
+
+    return {
+      init: init,
+      filter: function(query) {},
+      clearFilter: clearFilter
+    }
+
+  })(bbutler.Schematic, bbutler.Helpers);
+
+  bbutler.Main = (function(schematic, partList, helpers) {
+
+    var bindInvertButton = function() {
+      var invertEl = document.querySelector("#invert");
+      invertEl.addEventListener('click', function() {
+        helpers.toggleClass(document.documentElement, "inverted")
+      });
+    }
+
+    var init = function() {
+      schematic.assemble(partList.init);
+      bindInvertButton();
+    }
+
+    return {
+      init: init
+    }
+  })(bbutler.Schematic, bbutler.PartList, bbutler.Helpers);
+
+  document.addEventListener('DOMContentLoaded', bbutler.Main.init);
+
+  return bbutler;
+
+})(buildButler || {}, window, document);
