6161609f5e91c8bd0ed254d1ef89af06e2038974
Jacob Chapman <build@jakscoproductions.com>
2014-06-11 20:45:17 -0600

    Previous SVG file... scrollIntoView false?

--- bb/zglobal/js/bbutler.js
+++ bb/zglobal/js/bbutler.js
@@ -499,7 +499,7 @@ var buildButler = (function(bbutler, window, document) {
         if (previousSelection) helpers.removeClass(previousSelection, 'selectedpart');
         helpers.addClass(selected, 'selectedpart');
 
-        selected.scrollIntoView(true);
+        selected.scrollIntoView(false);
       });
 
       partList.addEventListener('click', function(e) {
