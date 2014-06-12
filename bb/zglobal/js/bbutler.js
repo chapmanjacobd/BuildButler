7aee2cd411a106b619ef5c3ad5acc59cb8a68e06
Jacob Chapman <build@jakscoproductions.com>
2014-06-11 21:34:26 -0600

    removed stroke and GPL license until sure of how we want to publish this...

--- bb/zglobal/js/bbutler.js
+++ bb/zglobal/js/bbutler.js
@@ -2,12 +2,6 @@
  * @overview bbutler.js
  * @copyright Jacob Chapman, Chris Chapman 2013-2014
  *
- * @license This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as published
- * by the Free Software Foundation version 3 of the License. This program is
- * distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE.
  */
 
 var buildButler = (function(bbutler, window, document) {
