b65c6f0c92fc430f39a39b3a30c146eb8f63b21c
Jacob Chapman <build@jakscoproductions.com>
2014-06-12 22:16:59 -0600

    updated default zoom added pretty restrictive permission to run the script...

--- bb/zglobal/js/bbutler.js
+++ bb/zglobal/js/bbutler.js
@@ -1,7 +1,22 @@
 /**
  * @overview bbutler.js
  * @copyright Jacob Chapman, Chris Chapman 2013-2014
- *
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software to execute the included software, without modification,
+ * to use, and copy, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 var buildButler = (function(bbutler, window, document) {
@@ -197,8 +212,9 @@ var buildButler = (function(bbutler, window, document) {
         buildFilename: 'build.svg',
         baseFilename: 'base.svg',
         svgPanZoomOptions: {
-          zoomScaleSensitivity: 0.15,
-          maxZoom: 8
+          zoomScaleSensitivity: 0.1,
+          maxZoom: 4,
+		  minZoom: .4
         }
       };
 
