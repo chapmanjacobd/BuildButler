7b2a171ad08d74b79d3c9641406eb95f83f027f6
Jacob Chapman <build@jakscoproductions.com>
2014-06-21 01:26:24 -0600

    updated colors hopefully for the last time. added beginnings of keyboard shortcut functionality.

--- bb/zglobal/js/bbutler.js
+++ bb/zglobal/js/bbutler.js
@@ -857,6 +857,77 @@ var buildButler = (function(window, document, svgPanZoom, bbutler) {
         schematic.selectComponentById(componentId);
       });
     };
+    
+    shortcut.add("W",function() {
+      //pan-up function
+    },{'type':'keypress','propagate': true,'disable_in_input':true});
+
+    shortcut.add("A",function() {
+      //pan-left function
+    },{'type':'keypress','propagate': true,'disable_in_input':true});
+
+    shortcut.add("S",function() {
+      //pan-down function
+    },{'type':'keypress','propagate': true,'disable_in_input':true});
+
+    shortcut.add("D",function() {
+      //pan-right function
+    },{'type':'keypress','propagate': true,'disable_in_input':true});
+
+    shortcut.add("Q",function() {
+      //zoom-in function
+    },{'type':'keypress','propagate': true,'disable_in_input':true});
+
+    shortcut.add("=",function() {
+      //same zoom-in function...
+    },{'type':'keypress','propagate': true,'disable_in_input':true});
+
+    shortcut.add("R",schematic.reset,{'type':'keypress','propagate': false,'disable_in_input':true});
+
+    shortcut.add("H",helpers.hideComponentList,{'type':'keypress','propagate': false,'disable_in_input':true});
+
+    shortcut.add("T",function() {
+      //select component above current selection
+    },{'type':'keypress','propagate': true,'disable_in_input':true});
+
+    shortcut.add("Up",function() {
+      //same select component above current selection function
+    },{'type':'keypress','propagate': true,'disable_in_input':true});
+
+    shortcut.add("G",function() {
+      //select component below current selection
+    },{'type':'keypress','propagate': true,'disable_in_input':true});
+
+    shortcut.add("Down",function() {
+      //same select component below current selection
+    },{'type':'keypress','propagate': true,'disable_in_input':true});
+
+    shortcut.add("Right",function() {
+      //select first component in next category and collapse current category
+    },{'type':'keypress','propagate': true,'disable_in_input':true});
+
+    shortcut.add("Left",function() {
+      //select first component in previous category and collapse current category
+    },{'type':'keypress','propagate': true,'disable_in_input':true});
+
+    shortcut.add("Z",function() {
+      //Toggle Between Current Selected & Previous Selected component
+    },{'type':'keypress','propagate': true,'disable_in_input':true});
+
+    shortcut.add("I",function() {
+      helpers.toggleClass(document.documentElement, 'inverted');
+    },{'type':'keypress','propagate': true,'disable_in_input':true}); 
+	//wahahaha emergency disco party...
+
+    shortcut.add("O",function monotonemode() { //hmm this isn't working
+      var svgid = document.querySelectorAll('svg [id^="_"]');
+
+      if ( svgid.style.fill != '#000' ) {
+      svgid.style.fill = '#000';
+      } else {
+      svgid.style.fill = '#000';
+	  }
+    },{'type':'keypress','propagate': true,'disable_in_input':true});
 
     var init = function(options) {
       schematic.assemble(options);
