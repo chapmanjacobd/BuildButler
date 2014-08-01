af65b2f57c986126c9adee0bbb4d8ead0761f5ff
jaksco <jakscoproductions@gmail.com>
2014-08-01 15:30:18 -0600

    this looks pretty good now

--- bb/js/BuildButler.js
+++ bb/js/BuildButler.js
@@ -2,23 +2,24 @@
  * @overview  BuildButler.js
  * @copyright Chris Chapman, Jacob Chapman 2013-2014
  * @url       http://buildbutler.org/
- * @version   1.0
+ * @version   1.1
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software ("the Software") to execute the Software, to use, and copy,
  * and to permit persons to whom the Software is furnished to do so, subject to
  * the following conditions:
  *
- * 1. All copies of the Software used with closed-source schematic files must
+ * 1. All copies of the Software used with closed-source design files must
  *    remain unmodified. Schematics with any part closed-source will be considered
- *    commercial and must be commercially licensed.
+ *    commercial and must hold a BuildButler commercial license.
+ *
  * 2. The above copyright notice and this permission notice shall be included in
  *    all copies of the Software.
  *
  * Permission is also granted, in addition to the above, to any person using a copy
- * of the Sofrware with only open-source schematic files (schematics which source 
- * files are freely available, at any time, in Gerber format), to modify, merge, 
- * publish, and distribute, the Software, subject to the above conditions.
+ * of the Sofrware with "Free Culture" design files (design source files are
+ * freely available, with no limitation, at any time, in Gerber format), to modify,
+ * merge, publish, and distribute, the Software, subject to the above conditions.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -266,7 +267,7 @@ var buildButler = (function(window, document, svgPanZoom, shortcut, bbutler) {
         var timeLapsed, percentage, position;
         var animationRequestID, animationStartTime;
 
-        var speed = 450; // How fast to complete the scroll in milliseconds
+        var speed = 1250; // How fast to complete the scroll in milliseconds
 
         var easeOutQuad = function(time) { return time * (2 - time); };
 
